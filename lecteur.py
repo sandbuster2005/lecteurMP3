ef4b951c73a6981c973aba057f68a2831d44fc57
sandbuster2005 <dorengebenjamin@gmail.com>
2025-02-16 16:15:52 +0100

    done some bug fixe here and there

--- lecteur.py
+++ lecteur.py
@@ -19,7 +19,7 @@ class App:
     from libs.handmade._image import init_image,get_img,display_img,select_img
     from libs.handmade._download import init_download,yt_search,dl_yt_playlist  
     from libs.handmade._song import init_song,choose_song,add_song,load_songs,play_song
-    from libs.handmade._playlist import init_playlist,create_playlist,load_playlist,select_playlist,get_psongs,edit_psong,show_psong
+    from libs.handmade._playlist import init_playlist,create_playlist,load_playlist,select_playlist,get_psongs,edit_psong
     from libs.handmade._main import init_main,main,update,get_input,display,help_menu,play,select,load_all,play_last,historic,wind
     from libs.handmade._letter import init_letter,suspend,a_f,b_f,c_f,d_f,e_f,f_f,g_f,h_f,i_f,j_f,k_f,l_f,m_f,n_f,o_f,p_f,q_f,r_f,s_f,t_f,u_f,v_f,w_f,x_f,y_f,z_f,plus_f,minus_f,dl_f,fv_f
 app=App()
